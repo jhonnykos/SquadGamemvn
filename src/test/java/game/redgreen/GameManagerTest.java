bc27ccae7534b93c001907111580a497ffc774eb
Olga <krylova.oa@mail.ru>
2021-11-03 13:40:57 +0300

    Add test with stop after moving for SpeedyGame

--- src/test/java/game/redgreen/GameManagerTest.java
+++ src/test/java/game/redgreen/GameManagerTest.java
@@ -14,8 +14,8 @@ class GameManagerTest {
         int[] speeds = {0, 0, 0, 0, 0, 1, 5, 5, 6};
         int expected = 5;
         int actual = manager.roundsWinCount(speeds);
-        Assertions.assertEquals(expected, actual, "Метод должен возвращать количество выигранных раундов" +
-                " при красном свете");
+        Assertions.assertEquals(expected, actual, "Метод должен возвращать количество выигранных раундов " +
+                "при красном свете");
     }
 
     @Test
@@ -25,8 +25,8 @@ class GameManagerTest {
         int[] speeds = {0, 0, 0, 0, 0, 1, 5, 0, 6};
         int expected = 5;
         int actual = manager.roundsWinCount(speeds);
-        Assertions.assertEquals(expected, actual, "Метод должен возвращать количество выигранных раундов" +
-                " до первого проигранного при красном свете");
+        Assertions.assertEquals(expected, actual, "Метод должен возвращать количество выигранных раундов " +
+                "до первого проигранного при красном свете");
     }
 
     @Test
@@ -36,8 +36,8 @@ class GameManagerTest {
         int[] speeds = {0, 0, 0, 0, 0, 1, 5, 0, 6};
         int expected = 9;
         int actual = manager.roundsWinCount(speeds);
-        Assertions.assertEquals(expected, actual, "Метод должен возвращать количество всех раундов" +
-                " при зеленом свете");
+        Assertions.assertEquals(expected, actual, "Метод должен возвращать количество всех раундов " +
+                "при зеленом свете");
     }
 
     @Test
@@ -47,8 +47,19 @@ class GameManagerTest {
         int[] speeds = {0, 0, 0, 0, 0, 1, 10, 15, 6};
         int expected = 7;
         int actual = manager.roundsWinCount(speeds);
-        Assertions.assertEquals(expected, actual, "Метод должен возвращать количество выигранных раундов" +
-                " при красном свете");
+        Assertions.assertEquals(expected, actual, "Метод должен возвращать количество выигранных раундов " +
+                "при красном свете");
+    }
+
+    @Test
+    void shouldReturnRoundsWinCountWithMaxSpeedIfStayAfterMoveRed() {
+        Game game = new SpeedyGame(false, 10);
+        GameManager manager = new GameManager(game);
+        int[] speeds = {0, 0, 0, 0, 0, 10, 11, 5, 16};
+        int expected = 6;
+        int actual = manager.roundsWinCount(speeds);
+        Assertions.assertEquals(expected, actual, "Метод должен возвращать количество выигранных раундов " +
+                "до первого проигранного при красном свете");
     }
 
     @Test
